2ecb4915073796469e6bcbb8b6cd87c57df99a50
pauloprescendo <paulovinicius1994@hotmail.com>
2019-11-14 15:40:54 -0300

    clear comments in index.js

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable quotes */
 function openOptions() {
   document.getElementById('options').style.width = '180px';
 }
@@ -7,16 +6,3 @@ function openOptions() {
 function closeOptions() {
   document.getElementById('options').style.width = '0';
 }
-
-// const $options = $('#options');
-
-// $(document).mouseup((e) => {
-//   if (!$options.is(e.target) && $options.has(e.target).length === 0) {
-//     $options.css({"display": "none"});
-//   }
-// });
-
-// $('#btn-options').click(() => {
-//   openOptions();
-//   $options.animate({minWidth: '180px'}).css({display: 'flex'});
-// });
