09575dd5d4ab362b54887040f2d65aed17b73a05
pauloprescendo <paulovinicius1994@hotmail.com>
2019-11-14 21:43:02 -0300

    fix option button

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -2,7 +2,13 @@ function openOptions() {
   document.getElementById('options').style.width = '180px';
 }
 
-
 function closeOptions() {
   document.getElementById('options').style.width = '0';
 }
+
+$('body,html').click((e) => {
+  var options = $('#btn-options');
+  if (!options.is(e.target) && $('#options').has(e.target).length === 0) {
+    closeOptions();
+  }
+});
